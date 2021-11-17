38a25145836199ef7b9d99e688ba65dd24a89b24
Isabella <isabellasa@al.insper.edu.br>
2021-11-17 19:04:42 -0300

    import questoes

---
+++ 01.py
@@ -0,0 +1,10 @@
+def one_to_fivemillion(): 
+    n_sum  = 0
+
+    for i in range(1, 5000001):
+        if (i % 2 == 0) and (i % 49 == 0) and (i % 37 == 0): 
+            n_sum += 1
+
+    return n_sum
+
+print(one_to_fivemillion())
---
+++ 02.py
@@ -0,0 +1,17 @@
+from math import *
+
+vetorx = [0] * 10
+
+for i in range(0, 10): 
+
+    if i % 2 == 0: 
+        vetorx[i] = 3 ** i + 7 * factorial(i)
+
+    else: 
+        vetorx[i] = 2 ** i + 4 * log(i)
+
+media = sum(vetorx)/10
+max_position = vetorx.index(max(vetorx))
+
+print('A média dos elementos desse vetor é: {:.2f}'. format(media))
+print('A posição do maior elemento desse vetor é: {}'. format(max_position))
